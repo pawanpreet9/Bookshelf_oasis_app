f44e0626a75c03935b98915de4ad2def8c8876fe
Pawanpreet9 <pawanpreetkaur911@gmail.com>
2023-07-30 18:45:49 -0700

    taking the address information and creating an order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,21 +6,27 @@ class OrdersController < ApplicationController
       @order.order_items.build(book_id: cart_item.book_id, quantity: cart_item.quantity)
     end
   end
-
   def create
     @order = Order.new(order_params)
-    @order.customer = current_customer
+
+    if current_customer
+
+      @order.customer = current_customer
+    else
+
+      @order.build_customer(customer_params)
+    end
 
     if @order.save
-      # Empty the cart after successful order creation
+
       current_cart.cart_items.destroy_all
       redirect_to @order, notice: 'Order was successfully created.'
     else
-      render :new
+      render :checkout
     end
   end
   def total_price
-    # Calculate the total price for the order items, including taxes
+
     order_items.sum { |item| item.book.price * item.quantity }
   end
   private
@@ -29,6 +35,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:customer_id, :address, :city, :province_id, :postal_code,
                                   order_items_attributes: [:book_id, :quantity])
+  end
+  def customer_params
+    params.require(:customer).permit( :email, :address, :city, :province_id, :postal_code)
   end
 
 end
