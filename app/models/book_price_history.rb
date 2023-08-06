e2436c4b5fe5ea2e178272c6a66782b533f475ce
pawanpreet Kaur <126494380+pawanpreet9@users.noreply.github.com>
2023-08-06 11:42:28 -0700

    Update book_price_history.rb

--- app/models/book_price_history.rb
+++ app/models/book_price_history.rb
@@ -2,4 +2,7 @@
 
 class BookPriceHistory < ApplicationRecord
   belongs_to :book
+  def self.ransackable_attributes(auth_object = nil)
+    ["book_id", "created_at", "id", "price", "updated_at"]
+  end
 end
