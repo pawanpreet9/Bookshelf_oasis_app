ebdf0772be9bdc767bcc2b7698f7fb0a599d3de5
pawanpreet Kaur <126494380+pawanpreet9@users.noreply.github.com>
2023-08-06 11:39:47 -0700

    Update order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,21 +5,58 @@ class OrderItem < ApplicationRecord
   belongs_to :book
    # New field to store the book price at the time of order placement
    belongs_to :book_price_history, optional: true
-   belongs_to :gst_rate_history, optional: true
-   belongs_to :pst_rate_history, optional: true
-   belongs_to :hst_rate_history, optional: true
+   belongs_to :tax_rate_history, class_name: 'TaxRateHistory', foreign_key: 'tax_rate_history_id', optional: true
 
    before_validation :set_historical_data, on: :create
+   def subtotal
+    historical_book_price || book.price * quantity
+  end
 
-   private
+  def tax_rates
+    historical_gst_rate ||= tax_rate_history&.gst_rate || current_tax_rate_history_for_order(order)&.gst_rate
+    historical_pst_rate ||= tax_rate_history&.pst_rate || current_tax_rate_history_for_order(order)&.pst_rate
+    historical_hst_rate ||= tax_rate_history&.hst_rate || current_tax_rate_history_for_order(order)&.hst_rate
+    [historical_gst_rate || 0, historical_pst_rate || 0, historical_hst_rate || 0]
+  end
 
-   def set_historical_data
-     self.book_price_at_order = book.price
-     self.gst_rate_at_order = book.province.tax_rate_histories.last&.gst_rate
-     self.pst_rate_at_order = book.province.tax_rate_histories.last&.pst_rate
-     self.hst_rate_at_order = book.province.tax_rate_histories.last&.hst_rate
-   end
+  def total_taxes
+    (subtotal * (tax_rates.sum / 100)).round(2)
+  end
+
+  def total_price
+    subtotal + total_taxes
+  end
+
+  private
+
+  def current_tax_rate_history_for_order(order)
+    return tax_rate_history if tax_rate_history.present?
+
+    customer_province = order.customer&.province
+    return nil unless customer_province
+
+    TaxRateHistory.for_province(customer_province).order(created_at: :desc).first
+  end
+
+  def default_tax_rate_history
+    TaxRateHistory.new(gst_rate: 0, pst_rate: 0, hst_rate: 0)
+  end
+
+  def set_historical_data
+    self.tax_rate_history ||= default_tax_rate_history
+    self.historical_gst_rate ||= self.tax_rate_history.gst_rate
+    self.historical_pst_rate ||= self.tax_rate_history.pst_rate
+    self.historical_hst_rate ||= self.tax_rate_history.hst_rate
+    self.historical_book_price ||= book.price
+  end
+
+
   def self.ransackable_attributes(auth_object = nil)
     ["book_id", "created_at", "id", "order_id", "quantity", "updated_at"]
   end
+  def self.ransackable_associations(auth_object = nil)
+    ["book", "book_price_history", "gst_rate_history", "hst_rate_history", "order", "pst_rate_history"]
+  end
+
 end
+
